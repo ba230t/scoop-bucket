302ee2b9baa03c60a587daabdf9776fb41fb1516
ba230t <ba230t@yahoo.co.jp>
2020-01-25 14:48:58 +0900

    move *.json to bucket/

--- bin/iyokan-alltest.ps1
+++
@@ -1,9 +0,0 @@
-$jsons = Get-ChildItem *.json
-foreach($json in $jsons){
-    $packageName =  [System.IO.Path]::GetFileNameWithoutExtension($json.name)
-
-    scoop install -a 64 $json.name
-    scoop uninstall --purge $packageName
-    scoop install -a 32 $json.name
-    scoop uninstall --purge $packageName
-}
